a305d45cfe3c672c3fb1ec914e5be64d92065a66
Parsec <sailcali@gmail.com>
2021-11-24 15:37:39 -0800

    Add solar production by billing cycle function

--- enphase.py
+++ enphase.py
@@ -5,6 +5,25 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
+BILLING_PERIODS = {2021: {'Jan': [19, 33], 'Feb': [18, 30], 'Mar': [19, 29],
+                          'Apr': [20, 33], 'May': [19, 29], 'Jun': [18, 30],
+                          'Jul': [20, 32], 'Aug': [18, 29], 'Sep': [17, 30],
+                          'Oct': [19, 32], 'Nov': [17, 29], 'Dec': [17, 30]},
+                   2022: {'Jan': [19, 33], 'Feb': [17, 29], 'Mar': [21, 32],
+                          'Apr': [20, 30], 'May': [19, 29], 'Jun': [20, 32],
+                          'Jul': [20, 30], 'Aug': [18, 29], 'Sep': [19, 32],
+                          'Oct': [18, 29], 'Nov': [16, 29], 'Dec': [16, 30]}}
+
+
+def production_during_billing_cycle(start_date, end_date):
+    db = create_engine(ENPHASE_DB_STRING)
+    with db.connect() as con:
+        sql = f"""SELECT * FROM production WHERE time > '{start_date}' AND time < '{end_date}';"""
+        result = con.execute(sql)
+        data = pd.DataFrame(result.fetchall(), columns=['time', 'production'])
+        data.set_index(['time'], inplace=True)
+    return data
+
 
 def get_production_data():
     db = create_engine(ENPHASE_DB_STRING)
@@ -48,7 +67,8 @@ def get_dates_from_user():
 
 def get_user_option():
     options = {1: 'Get Production by Dates', 2: 'Get All Production',
-               3: 'Get Top 30 All Time Production Periods'}
+               3: 'Get Top 30 All Time Production Periods', 4: 'Production During Billing Cycle'}
+
     for num in options:
         print(f'{num}. {options[num]}')
     option = int(input('Select and option:\n'))
@@ -70,4 +90,25 @@ if __name__ == '__main__':
         prod = unique_production.head(30)
         prod.sort_values(['time'], inplace=True)
         print(prod)
+    elif option == 4:
+        while True:
+            year = input('Which year are you looking for?\n')
+            try:
+                year = int(year)
+            except ValueError:
+                print('Year must be an integer!')
+                continue
+            month = input('Which month does the cycle end in?\n').capitalize()
 
+            if year in BILLING_PERIODS:
+                if month in BILLING_PERIODS[year]:
+                    break
+                else:
+                    print('Month must use format Mmm.')
+            else:
+                print('Billing period not available for that year')
+        date_str = f'{year}-{month}-{BILLING_PERIODS[year][month][0]}'
+        end_date = datetime.strptime(date_str, '%Y-%b-%d')
+        start_date = end_date - timedelta(days=BILLING_PERIODS[year][month][1])
+        data = production_during_billing_cycle(start_date, end_date)
+        print(f"Production was {data['production'].sum()/1000} kwh")
